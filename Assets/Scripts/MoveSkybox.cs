f05ef6a1126f8ba633d542bb7962502ec2933ac2
MrD00M <camilo.villegas@upb.edu.co>
2018-03-22 00:08:49 -0500

    + Agregado CineMachine. + Skybox ahora rota (rotateSkybox.cs creado en la camara). + Se agrego "MainCameraDesired" como transform deseado para la camara final. + Arreglos menores a sistemas de partículas + Sonido de Channeling listo para 3 versiones.

---
+++ Assets/Scripts/MoveSkybox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSkybox : MonoBehaviour {
+
+	public float rotateSpeed;
+
+	void Update() 
+	{
+
+		RenderSettings.skybox.SetFloat ("_Rotation", rotateSpeed * Time.time);
+		Debug.Log (RenderSettings.skybox.GetFloat("_Rotation"));
+	}
+
+
+}
