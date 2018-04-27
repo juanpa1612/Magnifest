e64d08ca6dc903ff8121769c0f7488b328f91b64
JuanPabloAmado <chumi1612@hotmail.com>
2018-04-27 09:27:51 -0500

    Photon Implementation #1 Create Room and Transform View in two players

---
+++ Assets/Scripts/PhotonTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonTest : Photon.PunBehaviour
+{
+
+
+	void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("Alpha_V01");
+	}
+    public override void OnConnectedToPhoton()
+    {
+        Debug.Log("Me he conectado a photon");
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Un usuario se ha unido al servidor");
+        var rOptions = new RoomOptions();
+        rOptions.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom("TestRoom", rOptions, TypedLobby.Default);
+    }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Cuarto creado");
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Me uní al cuarto");
+    }
+}
